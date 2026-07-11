a337e4122f5b9abd48714d916aa7d62d7bb4fb54
fulgoriapp <fulgoriapp@gmail.com>
2026-07-11 02:03:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,5 @@
-// fulgori Service Worker v1
 const CACHE = 'fulgori-v1';
-const ASSETS = [
-  '/fulgori/',
-  '/fulgori/index.html',
-  '/fulgori/privacy.html',
-  '/fulgori/icon-192.png',
-  '/fulgori/icon-512.png',
-  '/fulgori/favicon.ico'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() =>
-      caches.match('/fulgori/index.html')
-    ))
-  );
-});
+const ASSETS = ['/', '/index.html', '/privacy.html', '/icon-192.png', '/icon-512.png', '/favicon.ico'];
+self.addEventListener('install', e => {e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));});
+self.addEventListener('activate', e => {e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));});
+self.addEventListener('fetch', e => {e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html'))));});
